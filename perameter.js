a50a4c3d1fd63229632189345565f438a608c397
SH_SOHEL <sujauddoulasohel352@gmail.com>
2024-02-03 00:03:14 +0600

    function power add

---
+++ perameter.js
@@ -0,0 +1,16 @@
+function square(number){
+    console.log(number);
+    const square= number * number;
+    console.log('Square of the given number is:',square)
+}
+square(5);
+console.log('---------------------------');
+square(50);
+
+function add(num1,num2){
+    const sum= num1+num2;
+    console.log('The summation is:',sum);
+}
+add(50,78);
+console.log('------------------------');
+add(855,75);
