5bdf38558356a0de7510b31b6d8610330384df1a
SH_SOHEL <sujauddoulasohel352@gmail.com>
2024-02-02 23:48:59 +0600

    Info js  function

---
+++ function.js
@@ -0,0 +1,11 @@
+/**---------------Function declaration--------------- */
+function picnic(parameters) {
+    console.log('Must come at 8 am');
+    console.log('the market must be completed');
+    console.log('Starker should be cut and cleaned well');
+    console.log('Cooking should be finished by 2 pm');
+  }
+  /**---------------Function Called--------------- */
+
+  picnic();
+  
